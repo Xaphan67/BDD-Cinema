6476505d4b574947a6b0aee40f408bfca46803d9
Xaphan67 <cedric.falda@gmail.com>
2023-07-25 14:57:31 +0200

    Ajout style css header

--- view/template.php
+++ view/template.php
@@ -5,16 +5,24 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?= $title ?></title> <!-- Titre de la page généré dans la vue -->
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
 
 <body>
     <!-- Navigation -->
-    <nav>
-        <a href="index.php">Films</a>
-        <a href="index.php?action=listRealisateurs">Réalisateurs</a>
-        <a href="index.php?action=listActeurs">Acteurs</a>
-        <a href="index.php?action=listGenres">Genres</a>
-        <a href="index.php?action=listRoles">Rôles</a>
-    </nav>
-    <?= $contenu ?>
+    <header>
+        <div id="banner">
+            <img src="public/img/banner.png" alt="banner"></img>
+        </div>
+        <nav>
+            <a href="index.php">Films</a>
+            <a href="index.php?action=listRealisateurs">Réalisateurs</a>
+            <a href="index.php?action=listActeurs">Acteurs</a>
+            <a href="index.php?action=listGenres">Genres</a>
+            <a href="index.php?action=listRoles">Rôles</a>
+        </nav>
+    </header>
+    <main>
+        <?= $contenu ?>
+    </main>
 </body>
