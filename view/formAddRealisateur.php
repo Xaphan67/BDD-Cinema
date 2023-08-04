0c9c13d860546b53303457b347a5b0c0536119a3
Xaphan67 <cedric.falda@gmail.com>
2023-08-04 16:04:05 +0200

    Refonte CSS formulaires Ajouter / Modifier un réalisateur

--- view/formAddRealisateur.php
+++ view/formAddRealisateur.php
@@ -1,36 +1,40 @@
 <?php ob_start(); ?>
 
-<div class="main-box-titre bg">
+<div class="section">
     <h1>Ajouter un rélisateur</h1>
-    <p>Merci de remplir tous les champs pour ajouter un rélisateur.</p>
-    <form action="index.php?action=addRealisateur" method="post">
-        <div class="form-group">
-            <label for="nom">Nom *</label>
-            <input type="text" id="nom" name="nom" required>
-        </div>
-        <div class="form-group">
-            <label for="prenom">Prenom *</label>
-            <input type="text" id="prenom" name="prenom" required>
-        </div>
-        <div class="form-group">
-            <label for="sexe">Sexe *</label>
-            <select id="sexe" name="sexe" required>
-                <option value="Homme">Homme</option>
-                <option value="Femme">Femme</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="dateNaissance">Date de naissance *</label>
-            <input type="date" id="dateNaissance" name="dateNaissance" required>
-        </div>
-        <button type="submit" class="button-submit" name="submit">Ajouter</button>
-    </form>
+    <p>Merci de remplir tous les champs pour ajouter un rélisateur</p>
 </div>
+
+<article>
+    <div class="content content-no-bg">
+        <form action="index.php?action=addRealisateur" method="post">
+            <div class="form-group">
+                <label for="nom">Nom *</label>
+                <input type="text" id="nom" name="nom" required>
+            </div>
+            <div class="form-group">
+                <label for="prenom">Prenom *</label>
+                <input type="text" id="prenom" name="prenom" required>
+            </div>
+            <div class="form-group">
+                <label for="sexe">Sexe *</label>
+                <select id="sexe" name="sexe" required>
+                    <option value="Homme">Homme</option>
+                    <option value="Femme">Femme</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="dateNaissance">Date de naissance *</label>
+                <input type="date" id="dateNaissance" name="dateNaissance" required>
+            </div>
+            <button type="submit" name="submit">Ajouter</button>
+        </form>
+    </div>
+</article>
 
 <?php
 
-$titre = "Ajouter un rélisateur";
-$titre_secondaire = "Ajouter un rélisateur";
+$title = "Ajouter un rélisateur";
 $contenu = ob_get_clean();
 
 require "view/template.php";
--- view/formEditRealisateur.php
+++ view/formEditRealisateur.php
@@ -2,37 +2,41 @@
 
 $infosRealisateur = $realisateur->fetch(); ?>
 
-<div class="main-box-titre bg">
-    <h1>Modifier un rélisateur</h1>
-    <p>Merci de remplir tous les champs pour modifier le rélisateur.</p>
-    <form action="index.php?action=editRealisateur&id=<?= $infosRealisateur["id_realisateur"] ?>" method="post">
-        <div class="form-group">
-            <label for="nom">Nom *</label>
-            <input type="text" id="nom" name="nom" value="<?= $infosRealisateur["nom_personne"] ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="prenom">Prenom *</label>
-            <input type="text" id="prenom" name="prenom" value="<?= $infosRealisateur["prenom_personne"] ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="sexe">Sexe *</label>
-            <select id="sexe" name="sexe" required>
-                <option value="Homme" <?= $infosRealisateur["sexe_personne"] == "Homme" ? "selected" : "" ?>>Homme</option>
-                <option value="Femme" <?= $infosRealisateur["sexe_personne"] == "Femme" ? "selected" : "" ?>>Femme</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="dateNaissance">Date de naissance *</label>
-            <input type="date" id="dateNaissance" name="dateNaissance" value="<?= $infosRealisateur["dateNaissance_personne"] ?>" required>
-        </div>
-        <button type="submit" class="button-submit" name="submit">Modifier</button>
-    </form>
+<div class="section">
+    <h1>Modifier un réalisateur</h1>
+    <p>Merci de remplir tous les champs pour modifier le réalisateur</p>
 </div>
+
+<article>
+    <div class="content content-no-bg">
+        <form action="index.php?action=editActeur&id=<?= $infosRealisateur["id_realisateur"] ?>" method="post">
+            <div class="form-group">
+                <label for="nom">Nom *</label>
+                <input type="text" id="nom" name="nom" value="<?= $infosRealisateur["nom_personne"] ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="prenom">Prenom *</label>
+                <input type="text" id="prenom" name="prenom" value="<?= $infosRealisateur["prenom_personne"] ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="sexe">Sexe *</label>
+                <select id="sexe" name="sexe" required>
+                    <option value="Homme" <?= $infosRealisateur["sexe_personne"] == "Homme" ? "selected" : "" ?>>Homme</option>
+                    <option value="Femme" <?= $infosRealisateur["sexe_personne"] == "Femme" ? "selected" : "" ?>>Femme</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="dateNaissance">Date de naissance *</label>
+                <input type="date" id="dateNaissance" name="dateNaissance" value="<?= $infosRealisateur["dateNaissance_personne"] ?>" required>
+            </div>
+            <button type="submit" name="submit">Modifier</button>
+        </form>
+    </div>
+</article>
 
 <?php
 
-$titre = "Modifier le rélisateur";
-$titre_secondaire = "Modifier le rélisateur";
+$title = "Modifier le rélisateur";
 $contenu = ob_get_clean();
 
 require "view/template.php";
