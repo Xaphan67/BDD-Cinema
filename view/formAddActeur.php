641b4f31e642a8d83f40991aa964ad01e6353361
Xaphan67 <cedric.falda@gmail.com>
2023-08-04 15:56:18 +0200

    Refonte CSS formulaires Ajouter / Modifier un acteur

--- view/formAddActeur.php
+++ view/formAddActeur.php
@@ -1,36 +1,40 @@
 <?php ob_start(); ?>
 
-<div class="main-box-titre bg">
+<div class="section">
     <h1>Ajouter un acteur</h1>
-    <p>Merci de remplir tous les champs pour ajouter un acteur.</p>
-    <form action="index.php?action=addActeur" method="post">
-        <div class="form-group">
-            <label for="nom">Nom *</label>
-            <input type="text" id="nom" name="nom" required>
-        </div>
-        <div class="form-group">
-            <label for="prenom">Prenom *</label>
-            <input type="text" id="prenom" name="prenom" required>
-        </div>
-        <div class="form-group">
-            <label for="sexe">Sexe *</label>
-            <select id="sexe" name="sexe" required>
-                <option value="Homme">Homme</option>
-                <option value="Femme">Femme</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="dateNaissance">Date de naissance *</label>
-            <input type="date" id="dateNaissance" name="dateNaissance" required>
-        </div>
-        <button type="submit" class="button-submit" name="submit">Ajouter</button>
-    </form>
+    <p>Merci de remplir tous les champs pour ajouter un acteur</p>
 </div>
+
+<article>
+    <div class="content content-no-bg">
+        <form action="index.php?action=addActeur" method="post">
+            <div class="form-group">
+                <label for="nom">Nom *</label>
+                <input type="text" id="nom" name="nom" required>
+            </div>
+            <div class="form-group">
+                <label for="prenom">Prenom *</label>
+                <input type="text" id="prenom" name="prenom" required>
+            </div>
+            <div class="form-group">
+                <label for="sexe">Sexe *</label>
+                <select id="sexe" name="sexe" required>
+                    <option value="Homme">Homme</option>
+                    <option value="Femme">Femme</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="dateNaissance">Date de naissance *</label>
+                <input type="date" id="dateNaissance" name="dateNaissance" required>
+            </div>
+            <button type="submit" name="submit">Ajouter</button>
+        </form>
+    </div>
+</article>
 
 <?php
 
-$titre = "Ajouter un acteur";
-$titre_secondaire = "Ajouter un acteur";
+$title = "Ajouter un acteur";
 $contenu = ob_get_clean();
 
 require "view/template.php";
--- view/formEditActeur.php
+++ view/formEditActeur.php
@@ -2,37 +2,41 @@
 
 $infosActeur = $acteur->fetch(); ?>
 
-<div class="main-box-titre bg">
-    <h1>Modifier un acteur</h1>
-    <p>Merci de remplir tous les champs pour modifier l'acteur.</p>
-    <form action="index.php?action=editActeur&id=<?= $infosActeur["id_acteur"] ?>" method="post">
-        <div class="form-group">
-            <label for="nom">Nom *</label>
-            <input type="text" id="nom" name="nom" value="<?= $infosActeur["nom_personne"] ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="prenom">Prenom *</label>
-            <input type="text" id="prenom" name="prenom" value="<?= $infosActeur["prenom_personne"] ?>" required>
-        </div>
-        <div class="form-group">
-            <label for="sexe">Sexe *</label>
-            <select id="sexe" name="sexe" required>
-                <option value="Homme" <?= $infosActeur["sexe_personne"] == "Homme" ? "selected" : "" ?>>Homme</option>
-                <option value="Femme" <?= $infosActeur["sexe_personne"] == "Femme" ? "selected" : "" ?>>Femme</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="dateNaissance">Date de naissance *</label>
-            <input type="date" id="dateNaissance" name="dateNaissance" value="<?= $infosActeur["dateNaissance_personne"] ?>" required>
-        </div>
-        <button type="submit" class="button-submit" name="submit">Modifier</button>
-    </form>
+<div class="section">
+    <h1>Ajouter un acteur</h1>
+    <p>Merci de remplir tous les champs pour ajouter l'acteur</p>
 </div>
+
+<article>
+    <div class="content content-no-bg">
+        <form action="index.php?action=editActeur&id=<?= $infosActeur["id_acteur"] ?>" method="post">
+            <div class="form-group">
+                <label for="nom">Nom *</label>
+                <input type="text" id="nom" name="nom" value="<?= $infosActeur["nom_personne"] ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="prenom">Prenom *</label>
+                <input type="text" id="prenom" name="prenom" value="<?= $infosActeur["prenom_personne"] ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="sexe">Sexe *</label>
+                <select id="sexe" name="sexe" required>
+                    <option value="Homme" <?= $infosActeur["sexe_personne"] == "Homme" ? "selected" : "" ?>>Homme</option>
+                    <option value="Femme" <?= $infosActeur["sexe_personne"] == "Femme" ? "selected" : "" ?>>Femme</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="dateNaissance">Date de naissance *</label>
+                <input type="date" id="dateNaissance" name="dateNaissance" value="<?= $infosActeur["dateNaissance_personne"] ?>" required>
+            </div>
+            <button type="submit" name="submit">Modifier</button>
+        </form>
+    </div>
+</article>
 
 <?php
 
-$titre = "Modifier l'acteur";
-$titre_secondaire = "Modifier l'acteur";
+$title = "Modifier l'acteur";
 $contenu = ob_get_clean();
 
 require "view/template.php";
