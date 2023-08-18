e4983b012e51933a0db1fa5b63dbbac79294b965
Xaphan67 <cedric.falda@gmail.com>
2023-08-18 17:48:38 +0200

    Ajout titles sur liens boutons

--- view/formAddFilm.php
+++ view/formAddFilm.php
@@ -32,7 +32,11 @@
                         <?php }
                         ?>
                     </select>
-                    <a class="button casting-actions form-button button-round" href="index.php?action=formAddRealisateur">+</a>
+                    <a class="button casting-actions form-button button-round" href="index.php?action=formAddRealisateur" title="Ajouter un réalisateur">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                        </svg>
+                    </a>
                 </div>
             </div>
             <div class="form-group form-group-col form-genres">
@@ -46,14 +50,18 @@
                         <?php }
                         ?>
                     </select>
-                    <a class="button casting-actions form-button button-round" href="index.php?action=formAddGenre">+</a>
+                    <a class="button casting-actions form-button button-round" href="index.php?action=formAddGenre" title="Ajouter un genre">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                        </svg>
+                    </a>
                 </div>
             </div>
             <div class="form-group form-group-col form-affiche">
                 <label>Affiche *</label>
                 <div class="select-button">
                     <input type="file" id="affiche" name="affiche" required>
-                    <label for="affiche" class="button casting-actions">
+                    <label for="affiche" class="button casting-actions" title="Choisir une affiche">
                         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
                             <path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z" />
                         </svg>
